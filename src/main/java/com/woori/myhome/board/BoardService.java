d518cf1e23aff7184f37739965f9822625d8efdb
backtrackimmy <backtrackimmy@gmail.com>
2022-02-09 20:52:00 +0900

    commit

---
+++ src/main/java/com/woori/myhome/board/BoardService.java
@@ -0,0 +1,21 @@
+package com.woori.myhome.board;
+
+import java.util.List;
+
+import com.woori.myhome.comment.CommentDto;
+
+public interface BoardService {
+	List<BoardDto> getList(BoardDto dto);
+	void insert(BoardDto dto);
+	BoardDto getView(String id);
+	int getTotal(BoardDto dto);
+	void delete(String id);
+	void update(BoardDto dto);
+	void comment_insert(CommentDto dto);
+	List<CommentDto> getCommentList(CommentDto dto);
+	
+	CommentDto getCommentView(CommentDto dto);
+	void comment_update(CommentDto dto);
+	void comment_delete(CommentDto dto);
+		
+}
